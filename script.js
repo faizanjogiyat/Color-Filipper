bfc93b582f0024009dd2cf7ae8e29394f16cd0e9
faizanjogiyat <faizijogiyat@gmail.com>
2024-01-20 04:02:07 -0800

    minor changes done

---
+++ script.js
@@ -0,0 +1,12 @@
+// document.body.style.color =color[i]
+
+
+function color (){
+    console.log('in color function')
+    
+    let colors = ['red' , 'black' , 'purple' , 'yellow' ,'cyan' ,'blue']
+    for(let i = 0; i < colors.length; i++){
+        console.log(colors[i])
+    }
+
+}
