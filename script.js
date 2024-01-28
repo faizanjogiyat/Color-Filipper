38e85798f2aca283c6e2a2836317213bc63ba1b1
ALEEZAJOGIYAT <alizajogyat09@gmail.com>
2024-01-28 16:17:44 +0500

    color-filliper

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-// document.body.style.color =color[i]
+let colors = ['red', 'orange', 'purple', 'yellow', 'cyan', 'blue', 'gray', 'Blue', 'Blueviolet', 'Brown', 'Burlywood', 'Cadetblue', 'Chartreuse', 'Chocolate', 'Coral', 'Cornflowerblue', 'Cornsilk', 'Crimson', 'Cyan', 'Darkblue', 'Darkcyan', 'Darkgoldenrod', 'Darkgray', 'Darkgreen', 'Darkkhaki', 'Darkolivegreen', 'Darkorange', 'Darkorchid']
+function filipper() {
+  const random = Math.floor(Math.random() * colors.length);
+  console.log(random)
+  document.body.style.background = colors[random];
 
-
-function color (){
-    console.log('in color function')
-    
-    let colors = ['red' , 'black' , 'purple' , 'yellow' ,'cyan' ,'blue']
-    for(let i = 0; i < colors.length; i++){
-        console.log(colors[i])
-    }
-
+  let name = document.getElementsByTagName("span")[0]
+  name.innerHTML = colors[random]
+  console.log(name, colors[random], 'name');
 }
+
